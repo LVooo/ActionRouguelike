c4c25423ddfb9f5447c3e028096c9a61c1989360
LVooo <61344766+LVooo@users.noreply.github.com>
2022-12-10 11:48:11 +1100

    Added VCharacter & Setup Input
    Add:
    - Character class
    - MoveForward / Turn Input

---
+++ Source/ActionRouguelike/ActionRouguelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRouguelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGUELIKE_API AActionRouguelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
